18260b33dcefe1928090f3a385722a99db5673d4
Dushyanth Ganesan <dushyanth.ganesan@gmail.com>
2020-10-22 13:15:36 -0400

    link to gihub and styling

---
+++ index.js
@@ -0,0 +1,5 @@
+let github = document.getElementById('github');
+
+github.addEventListener('click', (event) => {
+    event.preventDefault();
+    window.location.href = 'https://github.com/dushyanthganesan';
